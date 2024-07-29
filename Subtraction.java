d564b332534d1b3a2dd3c303fd5a5258044434d7
Manikanta-cloud <gadamsettimanikanta888@gmail.com>
2024-07-29 15:39:39 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.subtraction;
+
+/**
+ *
+ * @author gadam
+ */
+public class Subtraction {
+
+    public static void main(String[] args)
+    {
+            int a=20;
+            int b=10;
+            int c=a-b;
+        
+        System.out.println("subtraction is:"+c);
+    }
+}
